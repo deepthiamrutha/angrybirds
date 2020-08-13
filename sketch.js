4eda4a3a982d9857d6ff17c426d5fb961b925cfa
deepthiamrutha <68592979+deepthiamrutha@users.noreply.github.com>
2020-08-13 15:32:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -36,18 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
-   
-    slingshot = new Slingshot(bird.body,{x:200,y:100});
-
-    
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    strokeWeight(4);
+    //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -65,12 +63,20 @@ function draw(){
 
     bird.display();
     platform.display();
-    
+    //log6.display();
     slingshot.display();    
 }
+
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
+
+
 function mouseReleased(){
     slingshot.fly();
+}
+function keyPressed(){
+    if(keyCode===32){
+    slingshot.attach(bird.body);
+    }
 }
