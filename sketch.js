58236fd33c7aafee4536e7ff1c940bbfb22f5256
deepthiamrutha <68592979+deepthiamrutha@users.noreply.github.com>
2020-08-21 15:46:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-var score=0;
+
 var gameState = "onSling";
+var bg = "sprites/bg1.png";
+var score = 0;
 
 function preload() {
-    getBackGround();
+    getBackgroundImg();
 }
 
 function setup(){
@@ -42,24 +44,30 @@ function setup(){
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
-    
 }
 
 function draw(){
-    if(backgroundImg){
-    background(backgroundImg);
-}
+    if(backgroundImg)
+        background(backgroundImg);
+    
+        noStroke();
+        textSize(35)
+        fill("white")
+        text("Score  " + score, width-300, 50)
+    
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -69,20 +77,14 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();
-    pig1.score();
-    pig3.score();
-    textSize(30);
-    fill (255);
-    text ("Score:"+score,width-300,50);
-    
-    
+    console.log(bird.body.speed);
+    slingshot.display();    
 }
 
 function mouseDragged(){
-    if (gameState!=="launched"){
+    //if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    }
+    //}
 }
 
 
@@ -92,23 +94,28 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       // slingshot.attach(bird.body);
+    if(keyCode === 32&&bird.body.speed<1){
+       slingshot.attach(bird.body);
+       bird.trajectory=[];
+       Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
     }
 }
- async function getBackGround(){
-     var response=await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
-     var responseJSON=await response.json();
-     var dateTime=responseJSON.datetime;
-     var hour =dateTime.slice(11,13);
-     var bg ;
-     if(hour>=06&&hour<=19){
-         bg="sprites/bg.png";
-     }
-     else{
-         bg="sprites/bg2.jpg";
-     }
-     backgroundImg=loadImage(bg);
 
-     console.log(backgroundImg);
- }
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg1.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
